937c56fd699135f746e4ba3e96144fb6e739b7b8
Joe Tsang <joetsang586@gmail.com>
2017-08-05 15:38:57 +0100

    initial commit

---
+++ homework/w01d05/app.js
@@ -0,0 +1,17 @@
+prompt ("Hello");
+
+console.log("Hello");
+var scissors = 1;
+var rock = 2;
+var paper = 3;
+
+var playerChoice = 0;
+var computerChoise = 0;
+
+playerchoice = prompt("Pick Scissors(1), Rock (2), Paper (3). Type 1, 2 or 3");
+
+alert(playerchoice);
+
+computerChoise = ((Math.random() * 10) + 3);
+
+alert(computerChoise);
