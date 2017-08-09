79acb75b54d20780c222d38f635627e9affa8534
Joe Tsang <joetsang586@gmail.com>
2017-08-09 16:33:43 +0100

    first commit with variables and suedo code

---
+++ homework/w02d03/js/index.js
@@ -0,0 +1,37 @@
+//Create Noughts and Crosses Game
+//Global variables
+var playerTurn = true;
+//playerTurn
+var winConditions = [];
+//array for win conditions
+//array for player actions
+var player1Actions = [];
+var player2Actions = [];
+//array of all boxes
+var boxes = $("td");
+
+$(boxes).each(function(index, box){
+  console.log($(box).attr("data-num"));
+})
+// console.log(boxes);
+
+//texts
+
+
+
+//Create listeners for all boxes
+
+
+
+
+//Create functions
+
+
+
+
+//change backgrounds for squares
+
+
+//compare win combinations
+
+//When no player wins
