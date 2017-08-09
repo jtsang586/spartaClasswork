75b9e123990380c97c8c9fc8f63bf7f27205189c
Joe Tsang <joetsang586@gmail.com>
2017-08-09 12:26:08 +0100

    finished code run for calc hwk

--- classwork/w02d03/dom_calc_starter/js/basicCalc.js
+++ classwork/w02d03/dom_calc_starter/js/basicCalc.js
@@ -17,12 +17,11 @@ var second_number = null;
 var operator = null;
 //variable for answer of calculations
 var answer = null;
-//---------------------------------------------------------------------
 //set up event listeners
 function setupEventListeners(){
   //numbers event listeners
   for (var i = 0; i < numbers.length; i++){
-    numbers[i].addEventListener("click", updateAndDisplayNum)
+    numbers[i].addEventListener("click", updateAndDisplayNumber)
   }
   //operators event listeners
   for (var i = 0; i < operators.length; i++){
@@ -35,36 +34,63 @@ function setupEventListeners(){
   //clear event listeners
   clear.addEventListener("click", clearCal)
 }
-
-
 //Functions for calculator
-
 //add
-
+function add(firstNumber, secondNumber){
+  return firstNumber + secondNumber;
+}
 //subtract
-
+function subtract (firstNumber, secondNumber){
+  return firstNumber - secondNumber;
+}
 //multiply
-
+function multiply (firstNumber, secondNumber){
+  return firstNumber * secondNumber;
+}
 //divide
-
+function divide (firstNumber, secondNumber){
+  return firstNumber / secondNumber;
+} 
 //---------------------------------------------------------------------
-
 //calculation
 function calculation(firstNumber, operator, secondNumber){
+  switch (operator) {
+    case "+":
+      display.value = add(firstNumber, secondNumber);
+      break;
+    case "-":
+      display.value = subtract(firstNumber, secondNumber);
+      break;
+    case "x":
+      display.value = multiply(firstNumber, secondNumber);
+      break;
+    case "/":
+      display.value = divide(firstNumber, secondNumber);
+      break;
+  }
 }
-
-
 //---------------------------------------------------------------------
-
 //clear the calculator
 function clearCal(){
+  first_number = null;
+  second_number = null;
+  operator = null;
+  display.value = "";
 }
 //display and store the buttons clicked
-function updateAndDisplayNum(){
-}
+function updateAndDisplayNumber(){
+  var btn = this.value;
+  display.value = btn;
 
+  if(first_number === null){
+    first_number = parseFloat(btn);  
+  } else {
+    second_number = parseFloat(btn);
+  }
+}
 //display and store the operator
 function updateAndDisplayOperator(){
+  operator = this.value;
+  display.value = operator;
 }
-
 setupEventListeners();
