7ebd9757e1667dc217e056fb785dc05417712d47
Joe Tsang <joetsang586@gmail.com>
2017-08-09 22:29:49 +0100

    added more functions

--- homework/w02d03/js/index.js
+++ homework/w02d03/js/index.js
@@ -1,12 +1,9 @@
 //Create Noughts and Crosses Game
 //Global variables
 var playerTurn = true;
-//playerTurn
-var winConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-//array for win conditions
-//array for player actions
-var player1WinConditions = winConditions;
-var player2WinConditions = winConditions;
+var counter = null;
+var player1WinConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+var player2WinConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 //array of all boxes
 var boxes= $("td");
 
@@ -14,42 +11,77 @@ $(boxes).each(function(index, box){
   console.log($(box).attr("data-num")); //data-num
   $(box).on("click", function(){
     if (playerTurn == true){
+    display("It is O's turn");
     var player1Choise = parseInt(($(box).attr("data-num")));
     console.log("player 1", player1Choise);  
     $(box).addClass("X").html("X");
     playerTurn = false; 
     player1WinConditions = checkWin(player1Choise, player1WinConditions);
+    console.log(player1WinConditions);
     }
     else if(playerTurn == false) { 
+    display("It is X's turn");
     var player2Choise = parseInt(($(box).attr("data-num")));
     console.log("player 2", player2Choise);  
     $(box).addClass("O").html("O");
     playerTurn = true; 
     player2WinConditions = checkWin(player2Choise, player2WinConditions);
+    console.log(player2WinConditions);
+
+    }
+    counter++
+    console.log(counter);
+    if (counter == 9){
+      draw();
     }
   })
 })
+//reset button
+
+$("#reset").on("click", clear)
 
 //Create functions
-
-function changePic(){
-
-}
 function checkWin (playerChoise, winConditions){
   for (var i = 0; i < winConditions.length; i++) {
     for (var j = 0; j < winConditions[i].length; j++) {
       if (winConditions[i][j] == playerChoise) {
         winConditions[i].splice(j,1);
-        console.log(winConditions);
+        // console.log(winConditions);
       }
       if (winConditions[i].length == 0){
+          win();
           console.log("Win!!!");
         }
     }
   }
+  // console.log(winConditions);
   return winConditions;
 }
 function draw(){
+  display("It's a draw!!!!!");
+}
 
+function win(){
+  if (playerTurn == false){
+    display("X has Won!!!!!");
+  } else {
+    display("O has Won!!!!!");
+  }
+}
+
+function display(text) {
+  $(".playerTurn").html(text);
+}
+
+function clear (){
+  var player1WinConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  var player2WinConditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+  var playerTurn = true;
+  var counter = 0;
+  console.log(counter);
+
+  $(boxes).each(function(index, box){
+    $(box).removeClass("X").removeClass("O").html("");
+  })
 }
 //When no player wins
