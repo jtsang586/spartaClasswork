5ccce9b2b3513cce4ba56bee1528b391d5fd4c23
Joe Tsang <joetsang586@gmail.com>
2017-08-21 21:25:22 +0100

    changed function

--- homework/w04d01/app.rb
+++ homework/w04d01/app.rb
@@ -41,60 +41,64 @@ def trip_calc distance, mpg, per_gallon, speed
   puts "Your trip will take #{time.round(2)}hrs and cost £#{cost.round(2)}."
 end
 
-puts "choose which calculator you want to use a) Basic b) Advanced c) BMI d) Trip Calculator"
-calculator_type = gets.chomp
+def start_calc 
+  puts "choose which calculator you want to use a) Basic b) Advanced c) BMI d) Trip Calculator"
+  calculator_type = gets.chomp
 
-if calculator_type == "a"
-  puts "pick a number"
-  number1 = gets.chomp.to_i
-
-  puts "Pick and operator 1)+ 2)- 3)* 4)/"
-  operator = gets.chomp
-
-  puts "Pick second number"
-  number2 = gets.chomp.to_i
-
-  if operator == "1"
-    puts add_number number1, number2
-  elsif operator == "2"
-    puts subtract_number number1, number2
-  elsif operator == "3"
-    puts multiply_number number1, number2
-  elsif operator == "4"
-    puts divide_number number1, number2
-  end
-elsif calculator_type == "b"
-  puts "pick a function for the calculator 1)Power 2)Square Root"
-  function = gets.chomp
-  if function == "1"
+  if calculator_type == "a"
     puts "pick a number"
     number1 = gets.chomp.to_i
 
-    puts "pick a second number"
+    puts "Pick and operator 1)+ 2)- 3)* 4)/"
+    operator = gets.chomp
+
+    puts "Pick second number"
     number2 = gets.chomp.to_i
-    puts power number1, number2
-  elsif function == "2"
-    puts "pick a number"
-    number1 = gets.chomp.to_i
-    puts square_root number1
+
+    if operator == "1"
+      puts add_number number1, number2
+    elsif operator == "2"
+      puts subtract_number number1, number2
+    elsif operator == "3"
+      puts multiply_number number1, number2
+    elsif operator == "4"
+      puts divide_number number1, number2
+    end
+  elsif calculator_type == "b"
+    puts "pick a function for the calculator 1)Power 2)Square Root"
+    function = gets.chomp
+    if function == "1"
+      puts "pick a number"
+      number1 = gets.chomp.to_i
+
+      puts "pick a number to power"
+      number2 = gets.chomp.to_i
+      puts power number1, number2
+    elsif function == "2"
+      puts "pick a number"
+      number1 = gets.chomp.to_i
+      puts square_root number1
+    end
+  elsif calculator_type == "c"
+    puts "enter your height(m or in)"
+    height = gets.chomp.to_f
+    
+    puts "enter your weight (kg or lb)"
+    weight = gets.chomp.to_f
+
+    puts BMI weight, height
+  elsif calculator_type == "d"
+    puts "enter distance"
+    distance = gets.chomp.to_f
+    puts "enter mpg"
+    mpg = gets.chomp.to_f
+    puts "enter cost per gallon"
+    per_gallon = gets.chomp.to_f
+    puts "enter speed"
+    speed = gets.chomp.to_f
+
+    trip_calc distance, mpg, per_gallon,speed    
   end
-elsif calculator_type == "c"
-  puts "enter your height(m or in)"
-  height = gets.chomp.to_f
-  
-  puts "enter your weight (kg or lb)"
-  weight = gets.chomp.to_f
-
-  puts BMI weight, height
-elsif calculator_type == "d"
-  puts "enter distance"
-  distance = gets.chomp.to_f
-  puts "enter mpg"
-  mpg = gets.chomp.to_f
-  puts "enter cost per gallon"
-  per_gallon = gets.chomp.to_f
-  puts "enter speed"
-  speed = gets.chomp.to_f
-
-  trip_calc distance, mpg, per_gallon,speed    
 end
+
+start_calc
