65719a7f888b7d135b5e39c6159947b944970d4d
Joe Tsang <joetsang586@gmail.com>
2017-08-29 19:23:22 +0100

    seeded the database and website working

--- homework/w05d01/fake_youtube/db/seeds.rb
+++ homework/w05d01/fake_youtube/db/seeds.rb
@@ -3,5 +3,8 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ Video.create ([
+    {title: 'God of War 4', description: "From Santa Monica Studio and creative director Cory Barlog comes a new beginning for one of gaming’s most recognizable icons. Living as a man outside the shadow of the gods, Kratos must adapt to unfamiliar lands, unexpected threats, and a second chance at being a father. Together with his son Atreus, the pair will venture into the brutal Norse wilds and fight to fulfill a deeply personal quest.", url: "gOE2BVRCUkM"},
+    {title: 'Shadow of The Collosus', description: "Here's the E3 2017 exclusive trailer for Shadow of the Colossus.", url: 'pdZQ98mWeto'},
+    {title: 'Monster Hunter World', description: "Monster Hunter: World™ introduces a living, breathing ecosystem in which players strive to become the ultimate hunter. Explore a vast new continent and slay terrifying monsters in heart-pounding co-op battles for up to four players.", url: 'SE_FnuD9zJc'}
+    ])
