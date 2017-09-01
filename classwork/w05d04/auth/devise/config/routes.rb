1b3e3976374aafa2f90532d175eaaef7a4c74043
Joe Tsang <joetsang586@gmail.com>
2017-09-01 09:31:20 +0100

    fixed some styling

--- classwork/w05d04/auth/devise/config/routes.rb
+++ classwork/w05d04/auth/devise/config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  root 'static#home'
 end
